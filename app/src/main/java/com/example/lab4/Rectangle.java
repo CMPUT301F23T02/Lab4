4fdcc13ea8e4160de55b0e60fb8a03b0981d7e7e
Ying-SunWan <ywan4@ualberta.ca>
2023-10-03 18:29:07 -0600

    Colored Rect Class

--- app/src/main/java/com/example/lab4/Rectangle.java
+++ app/src/main/java/com/example/lab4/Rectangle.java
@@ -3,4 +3,13 @@ package com.example.lab4;
 
 public abstract class Rectangle extends Shape {
 
+    int x;
+	int y;
+    String color = "blue";
+	
+	public Shape(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
 }
