9dee59a63615cc2600e1c0cbcae9db3702cede8e
Ying-SunWan <ywan4@ualberta.ca>
2023-10-05 23:40:51 -0600

    Forgot to save file after resolving merge

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -5,10 +5,6 @@ public abstract class Shape {
 
 	int x;
 	int y;
-<<<<<<< HEAD
     String color = "red";
-=======
-	String color = "$PUT_YOUR_CHOICE_OF_COLOR";
->>>>>>> e0c7e570843c73161599cfdbaf378cf79c6ebb93
 
 }
